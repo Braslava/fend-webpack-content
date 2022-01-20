5201849a4a19bd4166f35033379cac4da9e3767d
Your Name <m.braslava@gmail.com>
2022-01-20 12:14:18 +0200

    adds webpack html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
 const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
 	entry: './src/client/index.js',
 	module: {
 		rules: [
 			{
-				test: /.js$/,
+				test: /\.js$/,
 				exclude: /node_modules/,
 				loader: 'babel-loader',
 			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+					},
+				],
+			},
 		],
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/client/views/index.html',
+			filename: 'index.html',
+		}),
+	],
 };
 //output: { ...output options }
